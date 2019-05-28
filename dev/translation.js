4725cbc7512bce2c74aa554e75ec203678de6fe6
IchZerowan <Ih01@i.ua>
2019-05-28 21:27:07 +0300

    Uploaded project

---
+++ dev/translation.js
@@ -0,0 +1,32 @@
+Translation.addTranslation("Redstone Engine", {ru: "Двигатель на красном камне"});
+Translation.addTranslation("Stirling Engine", {ru: "Двигатель Стирлинга"});
+Translation.addTranslation("ICE", {ru: "ДВС"});
+Translation.addTranslation("Electric Engine", {ru: "Электрический двигатель"});
+Translation.addTranslation("Tank", {ru: "Цистерна"});
+Translation.addTranslation("Pump", {ru: "Помпа"});
+
+Translation.addTranslation("Wooden Transport Pipe", {ru: "Деревянная транспортная труба"});
+Translation.addTranslation("Cobblestone Transport Pipe", {ru: "Булыжниковая транспортная труба"});
+Translation.addTranslation("Stone Transport Pipe", {ru: "Каменная транспортная труба"});
+Translation.addTranslation("Sandstone Transport Pipe", {ru: "Песчаниковая транспортная труба"});
+Translation.addTranslation("Iron Transport Pipe", {ru: "Железная транспортная труба"});
+Translation.addTranslation("Golden Transport Pipe", {ru: "Золотая транспортная труба"});
+Translation.addTranslation("Obsidian Transport Pipe", {ru: "Обсидиановая транспортная труба"});
+Translation.addTranslation("Emerald Transport Pipe", {ru: "Изумрудная транспортная труба"});
+Translation.addTranslation("Diamond Transport Pipe", {ru: "Алмазная транспортная труба"});
+
+Translation.addTranslation("Wooden Fluid Pipe", {ru: "Деревянная жидкостная труба"});
+Translation.addTranslation("Cobblestone Fluid Pipe", {ru: "Булыжниковая жидкостная труба"});
+Translation.addTranslation("Stone Fluid Pipe", {ru: "Каменная жидкостная труба"});
+Translation.addTranslation("Iron Fluid Pipe", {ru: "Железная жидкостная труба"});
+Translation.addTranslation("Golden Fluid Pipe", {ru: "Золотая жидкостная труба"});
+Translation.addTranslation("Emerald Fluid Pipe", {ru: "Изумрудная жидкостная труба"});
+
+Translation.addTranslation("Wrench", {ru: "Гаечный ключ"});
+Translation.addTranslation("Wood Gear", {ru: "Деревянная шестерёнка"});
+Translation.addTranslation("Stone Gear", {ru: "Каменная шестерёнка"});
+Translation.addTranslation("Tin Gear", {ru: "Оловянная шестерёнка"});
+Translation.addTranslation("Iron Gear", {ru: "Железная шестерёнка"});
+Translation.addTranslation("Gold Gear", {ru: "Золотая шестерёнка"});
+Translation.addTranslation("Diamond Gear", {ru: "Алмазная шестерёнка"});
+Translation.addTranslation("Pipe Sealant", {ru: "Уплотнитель для труб"});
