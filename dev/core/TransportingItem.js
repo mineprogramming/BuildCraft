991c800a423aac77414320fa832af77706bf38d6
IchZerowan <Ih01@i.ua>
2019-06-06 11:04:35 +0300

    Fixed pipes connections

--- dev/core/TransportingItem.js
+++ dev/core/TransportingItem.js
@@ -35,7 +35,10 @@ var ItemTransportingHelper = {
     canPipesConnect: function(pipe1, pipe2){
         var type1 = this.PipeTiles[pipe1] || ITEM_PIPE_CONNECTION_ANY;
         var type2 = this.PipeTiles[pipe2] || ITEM_PIPE_CONNECTION_ANY;
-        return type1 == type2 || type1 == ITEM_PIPE_CONNECTION_ANY || type2 == ITEM_PIPE_CONNECTION_ANY;
+        return type1 == type2 && type1 != ITEM_PIPE_CONNECTION_WOOD 
+            || type1 == ITEM_PIPE_CONNECTION_ANY || type2 == ITEM_PIPE_CONNECTION_ANY
+            || type1 == ITEM_PIPE_CONNECTION_STONE && type2 != ITEM_PIPE_CONNECTION_COBBLE
+            || type1 == ITEM_PIPE_CONNECTION_COBBLE && type2 != ITEM_PIPE_CONNECTION_STONE;
     },
     
     canTransportTo: function(pipe, x, y, z){
