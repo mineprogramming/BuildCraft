2619f83d241b5ba183ea850794708ff5f7fdace0
Nikolay Savenko <savenkons@yandex.ru>
2020-07-20 12:07:59 +0300

    added declarations folder to root

---
+++ declarations/lib/numbers.d.ts
@@ -0,0 +1,207 @@
+/**
+ * numbers.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var numbers: {};
+/**
+ * basic.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var basic: {};
+/**
+ * calculus.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var calculus: {};
+/**
+ * Helper function in calculating integral of a function
+ * from a to b using simpson quadrature.
+ *
+ * @param {Function} math function to be evaluated.
+ * @param {Number} point to initiate evaluation.
+ * @param {Number} point to complete evaluation.
+ * @return {Number} evaluation.
+ */
+declare function SimpsonDef(func: any, a: any, b: any): number;
+/**
+ * Helper function in calculating integral of a function
+ * from a to b using simpson quadrature.  Manages recursive
+ * investigation, handling evaluations within an error bound.
+ *
+ * @param {Function} math function to be evaluated.
+ * @param {Number} point to initiate evaluation.
+ * @param {Number} point to complete evaluation.
+ * @param {Number} total value.
+ * @param {Number} Error bound (epsilon).
+ * @return {Number} recursive evaluation of left and right side.
+ */
+declare function SimpsonRecursive(func: any, a: any, b: any, whole: any, eps: any): any;
+/**
+ * complex.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var Complex: (re: any, im: any) => void;
+/**
+ * dsp.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var dsp: {};
+/**
+ * generators.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski, Kartik Talwar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var generate: {};
+/**
+ * matrix.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var matrix: {};
+declare var ERROR_MATRIX_NOT_SQUARE: string, ERROR_VECTOR_NOT_2D: string;
+/**
+ * Add all of the elements in an array together except for the i'th one.
+ * This is a helper function for determining diagonal dominance, and it
+ * should be noted that each element is passed to Math.abs() beforehand.
+ *
+ * @param {Array} array
+ * @param {Int} index of element to ignore.
+ * @return {Number} sum.
+ */
+declare var sumNondiagonalElements: (arr: any, i: any) => number;
+/**
+ * prime.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var prime: {};
+declare var random: {};
+declare var rGen: () => number;
+/**
+ * statistic.js
+ * http://github.com/sjkaliski/numbers.js
+ *
+ * Copyright 2012 Stephen Kaliski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+declare var statistic: {};
