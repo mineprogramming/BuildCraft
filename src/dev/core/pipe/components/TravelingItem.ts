91b1f5ebab88c620138937bad59f519f12fea2e5
Nikolay Savenko <savenkons@yandex.ru>
2020-08-11 00:13:03 +0300

    basic saver for TravelingItem

--- src/dev/core/pipe/components/TravelingItem.ts
+++ src/dev/core/pipe/components/TravelingItem.ts
@@ -7,6 +7,27 @@ type ItemSource = {
 class TravelingItem {
     //! its just a Updatable flag
     public remove: boolean = false;
+    public static saverId = Saver.registerObjectSaver("TravelingItemSaver", {
+        save: function(travelingItem) {
+            Debug.m(`im saved!`);
+            return {
+                coords: travelingItem.coords,
+                moveVector: travelingItem.moveVector,
+                moveSpeed: travelingItem.moveSpeed,
+                item: travelingItem.item
+            }
+        },
+
+        read: function(scope) {
+            Debug.m(`im readed!`);
+            const {x, y, z} = scope.coords;
+            const item = new TravelingItem(x, y, z, scope.item);
+            item.moveVector = scope.moveVector;
+            item.moveSpeed = scope.moveSpeed;
+            Updatable.addUpdatable(item);
+            return item
+        }
+    });
 
     private coords: Vector;
     public moveVector: Vector;
@@ -17,12 +38,15 @@ class TravelingItem {
             y: y,
             z: z
         }
+        Saver.registerObject(this, TravelingItem.saverId);
+        Updatable.addUpdatable(this);
         Debug.m(`item created on coords`);
         Debug.m(this.coords);
     }
 
     // * We need this to pass this["update"] existing
     public update = () => {
+        Debug.m(`update of ${this.item.id}`);
         if (!this.isInsideBlock()) {
             this.drop();
             return;
@@ -31,6 +55,7 @@ class TravelingItem {
     }
 
     private move() {
+        if (!(this.moveVector && this.moveSpeed)) return;
         this.coords.x += this.moveVector.x * this.moveSpeed;
         this.coords.y += this.moveVector.y * this.moveSpeed;
         this.coords.z += this.moveVector.z * this.moveSpeed;
@@ -45,3 +70,6 @@ class TravelingItem {
         this.remove = true;
     }
 }
+Callback.addCallback("ItemUse", (coords, item, block) => {
+    new TravelingItem(coords.x, coords.y, coords.z, item);
+});
