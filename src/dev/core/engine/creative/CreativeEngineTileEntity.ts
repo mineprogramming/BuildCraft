925d0b095729abc524f8ec025f4e6aa97b5041ee
Nikolay Savenko <savenkons@yandex.ru>
2020-10-20 23:13:06 +0300

    Creative engine multiplayer support

--- src/dev/core/engine/creative/CreativeEngineTileEntity.ts
+++ src/dev/core/engine/creative/CreativeEngineTileEntity.ts
@@ -1,11 +1,32 @@
 /// <reference path="../PowerMode.ts" />
 class BCCreativeEngineTileEntity extends BCEngineTileEntity {
-    //  private PowerMode powerMode = PowerMode.M2; // ! its from PC
     public energyStage = EngineHeat.BLACK;
 
     constructor(protected texture: EngineTexture){
         super(texture);
         this.defaultValues.powerMode = PowerMode.M2;
+        this.client.getEngineTexture = (stage: EngineHeat) => {
+            return EngineTextures.creative;
+        };
+        this.client.getPistonSpeed = function(energyStage: EngineHeat) {
+            // @ts-ignore
+            return 0.02 * (this.powerModeIndex+1);
+        }
+        // @ts-ignore
+        this.client._load = this.client.load;
+        this.client.load = function(){
+            this._load();
+            this.powerModeIndex = 0;
+            this.networkData.addOnDataChangedListener((networkData, isExternalChange) => {
+                // @ts-ignore
+                this.powerModeIndex = networkData.getInt("powerModeIndex");
+            });
+        }
+    }
+
+    public init(){
+        super.init();
+        this.syncPowerMode();
     }
 
     public click(id: number, count: number, data: number) {
@@ -15,11 +36,19 @@ class BCCreativeEngineTileEntity extends BCEngineTileEntity {
             this.data.energy = 0;
             let currentModeIndex = PowerModeOrder.indexOf(this.data.powerMode);
             this.data.powerMode = PowerModeOrder[++currentModeIndex % PowerModeOrder.length];
+            this.syncPowerMode();
             Game.tipMessage(`Mode switched to ${this.data.powerMode}RF`);
             return true;
         }
         this.setOrientation(this.getConnectionSide(true));
         return false;
+    }
+
+    private syncPowerMode(): void {
+        // @ts-ignore
+        this.networkData.putInt("powerModeIndex", PowerModeOrder.indexOf(this.data.powerMode));
+        // @ts-ignore
+        this.networkData.sendChanges();
     }
 
     protected computeEnergyStage(): EngineHeat {
@@ -29,8 +58,7 @@ class BCCreativeEngineTileEntity extends BCEngineTileEntity {
     public updateHeat(): void {}
 
     public getPistonSpeed(): number {
-       // return 0.02 * (powerMode.ordinal() + 1); // ORIGINAL
-       return 0.02 * (PowerModeOrder.indexOf(this.data.powerMode) + 1); // Maybe shit...
+       return 0.02 * (PowerModeOrder.indexOf(this.data.powerMode) + 1);
     }
 
     public engineUpdate(): void {
@@ -50,7 +78,6 @@ class BCCreativeEngineTileEntity extends BCEngineTileEntity {
     }
 
     public getIdealOutput(): number {
-        // return powerMode.maxPower; //ORIGINAL
         return this.data.powerMode;
     }
 }
