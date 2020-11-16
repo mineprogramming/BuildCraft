c98cc30dd3d09ab603f97c8eccdd666d3a4cf74f
Nikolay Savenko <savenkons@yandex.ru>
2020-11-16 14:41:37 +0300

    correct names and translations

--- src/dev/item/wrench.ts
+++ src/dev/item/wrench.ts
@@ -1,5 +1,5 @@
 IDRegistry.genItemID("bc_wrench");
-Item.createItem("bc_wrench", "Wrench", { name: "bc_wrench" }, { stack: 1 });
+Item.createItem("bc_wrench", "bc_wrench", { name: "bc_wrench" }, { stack: 1 });
 
 Callback.addCallback("PostLoaded", () => {
     Recipes.addShaped({ id: ItemID.bc_wrench, count: 1, data: 0 }, [
